554df6b7058934298762df1ca37fa199ee8fc1d0
wvxbs <gabriel.ferreira7854@gmail.com>
2022-10-07 22:12:55 -0300

    initial commit

---
+++ src/main/java/uam/springdemo/SpringBootController.java
@@ -0,0 +1,18 @@
+package uam.springdemo;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SpringBootController {
+    @RequestMapping("/hello")
+    public String index() {
+        return "<h1>Hello World</h1>";
+    }
+    
+    @RequestMapping("/cadastro/{nome}")
+    public String dizernome (@PathVariable String nome) {
+        
+    }
+}
